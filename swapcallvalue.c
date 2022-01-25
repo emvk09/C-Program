3cdc2f7a6efb8ff62b30918925dca876344eb359
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-25 15:15:52 +0530

    Update swapcallvalue.c

--- swapcallvalue.c
+++ swapcallvalue.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 
 // function declaration
-
 int swap(int, int);  
 
 int main()
 {
-    int a,b;
-    a=10;b=20;
-    printf("a=%d,b=%d\n",a,b);
+    int a, b;
+    a= 10;
+    b= 20;
+    printf("Before in main: a= %d, b= %d\n", a, b);
 
 // function call
+    swap(a, b);
 
-    swap(a,b);
-    printf("a=%d,b=%d",a,b);
+    printf("After in main: a= %d, b= %d", a, b);
     return 0;
 }
 
 //function definition
-
 int swap(int x, int y)
 {
     int t;
-    t=x;
-    x=y;
-    y=t;
+    t= x;
+    x= y;
+    y= t;
+    printf("In Function: x= %d, y= %d\n", x, y);
 }

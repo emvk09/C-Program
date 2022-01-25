02289ba95d8332fc3fd539c61c9c61c3aca9f3a0
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-25 15:40:22 +0530

    Update swapcallreference.c

--- swapcallreference.c
+++ swapcallreference.c
@@ -10,6 +10,8 @@ int main()
     b= 20;
     printf("Before in main: a= %d, b= %d\n", a, b);
 
+    // printf("%p\n", &a); /*Prints address of a  */
+
     // function call
     swap(&a, &b); /* the address of a and b is passed to pointers x and y */
 
@@ -25,4 +27,9 @@ int swap(int *x, int *y)
     *x= *y;  /* The swapping done here affects the actual parameter's value */
     *y= t;
     printf("In Function: x= %d, y= %d\n", *x, *y);
+
+    // printf("%p\n", x); /*Prints the address that pointer x points to (same as the address of a)  */
+    // printf("%p\n", *x); /*Prints the address of pointer x  */
+    // printf("%d\n", x);  /*Prints the dump value of the pointer x  */
+    // printf("%d\n", *x); /*Prints the value that pointer x points to (same as the swapped value of a)  */
 }

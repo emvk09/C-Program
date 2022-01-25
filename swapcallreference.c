351880658f4671abfdad88bd108396d86db80715
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-25 15:15:08 +0530

    Update swapcallreference.c

--- swapcallreference.c
+++ swapcallreference.c
@@ -1,31 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // function declaration
-
-void swap(int*, int*);  
-
-//function definition
-
-void swap(int*x, int*y)
-{
-    int t;
-    t=*x;
-    *x=*y;
-    *y=t;
-}
+int swap(int *, int *);  /*  we can also write void swap(int *x, int *y);  */
 
 int main()
 {
     int a, b;
-    a= 10; b= 20;
-    printf("a=%d,b=%d",a,b);
+    a= 10; 
+    b= 20;
+    printf("Before in main: a= %d, b= %d\n", a, b);
 
-                                            // function call
-    int*x= & a;  
-    int*y= & b;
-    swap(x,y);
-    // swap( &a , &b );
-    /*     Instead of 24 25 and 26 we can write : swap( &a , &b );        */
-    printf("a=%d,b=%d",a,b);
+    // function call
+    swap(&a, &b); /* the address of a and b is passed to pointers x and y */
+
+    printf("After in main: a= %d, b= %d", a, b);
     return 0;
+}
+
+//function definition
+int swap(int *x, int *y)
+{
+    int t;
+    t= *x;  /* here the value at pointer x (which actually points to the value stored in a) is copied to the int temp. */
+    *x= *y;  /* The swapping done here affects the actual parameter's value */
+    *y= t;
+    printf("In Function: x= %d, y= %d\n", *x, *y);
 }

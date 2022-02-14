3fa5b1b636ef9b3f6d50fc9975658e627bbc626d
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-02-15 01:06:21 +0530

    Update binarytree.c

--- binarytree.c
+++ binarytree.c
@@ -24,8 +24,8 @@ int main()
         scanf("%d", & num);
 
         // Here we are giving the address of the pointer (&root) rather than simply writing (root) because, 
-        // if we write root without &, it will simply means that the root pointer is passed as call by value.
-        // So this doesnot make any change in the actual node. Changes will be made only inside the insert() function.
+        // if we write root without &, it will simply mean that the root pointer is passed as call by value.
+        // So this doesnot make any changes in the main function. Changes will be made only inside the insert() function.
         // So inorder to do CALL BY REFERENCE, we are passing the address of the pointer; ie &root 
         insert(&root, num);
         printf("To continue Enter 1, else Enter 0: ");
@@ -35,6 +35,7 @@ int main()
 
     // Here we are simply writing (root) instead of (&root) because, in inorder(), pre order() and postorder() function, we are not making 
     // any changes in the actual arrangement of the struct nodes. We are simply displaying it with temporary arrangement of the nodes, only inside the function
+    // In short, these functions doesn't have any effect on the main function. So call by reference is not needed
     // Here CALL BY VALUE is being used.
     printf("Preorder: ");
     preOrder(root);
@@ -44,11 +45,11 @@ int main()
     postOrder(root);
 }
 
-// here rt is taken as a double pointer since it points to another pointer called root. 
+// here rt is taken as a double pointer since CALL BY REFERENCE is used from the main function 
 void insert(struct Node **rt, int x)
 {
-    // Here, if it was rt == 0 or rt->left etc, it would mean error since the double pointer rt holds the address of the root pointer. But what we 
-    // need is the address of the struct node. 
+    // Here, if it was rt == 0 or rt->left etc, it would mean error since the double pointer rt holds the address of the root pointer. 
+    // But what we need is the address of the struct node. 
     // rt: has the address of the root pointer 
     // *rt: has the address of root pointer and along with that, the root pointer has the address of struct node 
     // In short, rt points to root pointer, root points to struct node; and *rt directly points to the struct node
@@ -70,6 +71,7 @@ void insert(struct Node **rt, int x)
    }
 }
 
+// here rt is taken as a single pointer since CALL BY VALUE is used from the main function 
 void preOrder(struct Node *rt)
 {
     if (rt != 0)

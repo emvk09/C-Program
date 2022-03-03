50e3e309e943f13f5206f7ef3a813a2f1bb20498
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-03-03 17:04:01 +0530

    Update polynomialadd.c

--- polynomialadd.c
+++ polynomialadd.c
@@ -1,27 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h>
 #define N 10
-float add (float[], float[], int, int);
+int add(int[], int[], int, int);
 
 int main()
 {
-    int deg1, deg2, i;
-    float pol1[N], pol2[N];
+    int deg1, deg2, pol1[N], pol2[N], i;
+
     printf("Enter the degree of the 1st polynomial: ");
     scanf("%d", & deg1);
     printf("Enter the degree of the 2nd polynomial: ");
     scanf("%d", & deg2);
-    printf("Enter the coefficients of the 1st polynomial from the lowest order: \n");
-    for(i= 0; i<= deg1; i++)
-        scanf("%f", & pol1[i]);
-    printf("Enter the coefficients of the 2nd polynomial from the lowest order: \n");
-    for(i= 0; i<= deg2; i++)
-        scanf("%f", & pol2[i]);
+    printf("Enter the coefficients of the 1st polynomial: \n");
+    for(i= deg1; i>= 0; i--)
+    {
+        printf("x ^ %d: ", i);
+        scanf("%d", & pol1[i]);
+    }
+    printf("Enter the coefficients of the 2nd polynomial: \n");
+    for(i= deg2; i>= 0; i--)
+    {
+        printf("y ^ %d: ", i);
+        scanf("%d", & pol2[i]);
+    }
 
     add (pol1, pol2, deg1, deg2);
 }
 
-float add (float arr1[], float arr2[], int d1, int d2)
+int add(int arr1[], int arr2[], int d1, int d2)
 {
     int j, mindeg, maxdeg, resdeg;
     maxdeg= (d1 > d2) ? d1: d2;
@@ -38,17 +44,19 @@ float add (float arr1[], float arr2[], int d1, int d2)
             break;
     }
 
-    float respol [resdeg + 1];
+    // initialising the resultant array
+    int respol [resdeg + 1];
 
+    // adding upto same no. of terms 
     for(j= 0; j<= mindeg; j++)
         respol[j]= arr1[j]+ arr2[j];
-    
+
+    // copying the remaining terms
     if (d1 >= d2)
     {
         for(j= mindeg+ 1; j<= maxdeg; j++)
             respol[j]= arr1[j];
     }
-
     else
     {
         for(j= mindeg+ 1; j<= maxdeg; j++)
@@ -56,8 +64,8 @@ float add (float arr1[], float arr2[], int d1, int d2)
     }
 
     printf("\nThe sum of the polynomial is:\n");
-    for(j= 0; j<= resdeg; j++)
-        printf("%.2f\t", respol[j]);
-    
+    for(j= resdeg; j>= 0; j--)
+        printf("%dz^%d\t", respol[j], j);
+
     return 0;
 }
